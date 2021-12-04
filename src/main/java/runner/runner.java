f8f45b3c913a72f7f6bd223f2235bcfbdc0e513b
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-12-04 15:58:51 -0700

    add run function

--- src/main/java/runner/runner.java
+++ src/main/java/runner/runner.java
@@ -11,4 +11,20 @@ public class runner {
                 .collect(Collectors.joining());
         return raw;
 	}
+	
+	public static void run(String[] lines) {
+		private String line = "";
+		if ( bin2str(bline) == "SEEK" ) {
+			for (String bline : lines) {
+				line = bin2str(line);
+				if ( line.split(" ")[0] == "print" ) {
+					System.out.println(line.split(" ")[1]);
+				} else {
+					System.out.println("Invalid code");
+					System.exit("50")
+				}
+			}
+		}
+	}
+	
 }
