4486240e6c02907720fcd11b2e107cfd118e60dc
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-12-04 17:28:38 -0700

    finish cli

--- src/main/java/command/main.java
+++ src/main/java/command/main.java
@@ -5,11 +5,24 @@ import comp.compiler;
 import runner.runner;
 import fileread.filereader;
 
+
+private static String[] lines = filereader.read("test.seek");
+
 public class main {
     public static void main(String[] args) {
-        String[] lines = filereader.read("test.seek");
-        compiler.compile(lines, "test.seekc");
-        lines = filereader.read("test.seekc");
-        runner.run(lines);
+        if ( args.length == 0 ) {
+            System.out.println()
+        }
+        if ( args[0].equals("comp") ) {
+            if ( args.length > 3 || args.length < 3 ) {
+                lines = filereader.read(args[1]);
+                compiler.compile(lines, args[2]);
+            }
+        } else if ( args[0].equals("run") ) {
+            if ( args.length > 2 || args.length < 2 ) {
+                lines = filereader.read(args[1]);
+                runner.run(lines);
+            }
+        }
     }
 }
