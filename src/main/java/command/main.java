f3f5c7e6e369972be70a062efd240b6c35163730
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-12-04 18:08:49 -0700

    Update main.java

--- src/main/java/command/main.java
+++ src/main/java/command/main.java
@@ -15,15 +15,11 @@ public class main {
             System.exit(1);
         }
         if ( args[0].equals("comp") ) {
-            if ( args.length > 3 || args.length < 3 ) {
-                lines = filereader.read(args[1]);
-                compiler.compile(lines, args[2]);
-            }
+            lines = filereader.read(args[1]);
+            compiler.compile(lines, args[2]);
         } else if ( args[0].equals("run") ) {
-            if ( args.length > 2 || args.length < 2 ) {
-                lines = filereader.read(args[1]);
-                runner.run(lines);
-            }
+            lines = filereader.read(args[1]);
+            runner.run(lines);
         } else {
             System.out.println("Invalid arguments");
             System.exit(1);
