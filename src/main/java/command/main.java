52a6eff4c875bed90caa8e9918a5000657af00c4
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-12-04 17:50:01 -0700

    Update main.java

--- src/main/java/command/main.java
+++ src/main/java/command/main.java
@@ -8,7 +8,7 @@ import fileread.filereader;
 
 
 public class main {
-    String[] lines;
+    static String[] lines;
     public static void main(String[] args) {
         if ( args.length == 0 ) {
             System.out.println("Invalid arguments");
